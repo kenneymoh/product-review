7d75df0d116e12353845335f69d2e988e0c75ae6
kennedy <kenneyrit@gmail.com>
2023-02-27 11:18:30 +0300

    created the migrations

---
+++ db/migrate/20230227081404_create_reviews.rb
@@ -0,0 +1,9 @@
+class CreateReviews < ActiveRecord::Migration[7.0]
+  def change
+    create_table :reviews do |t|
+      t.integer :star_rating
+      t.string :comments
+      t.integer :product_id
+      t.integer :user_id
+  end
+end
